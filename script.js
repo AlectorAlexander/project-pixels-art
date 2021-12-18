d0f9f46dd1cc29c3ad004a58dc45c404937adeb1
Aléctor Alexander <alector.alexandre@gmail.com>
2021-12-18 18:07:19 -0300

    até o 5

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//Requisito2 e Requisito3
 
 let colorPalette = document.getElementById("color-palette")
 let cores = ["black","yellow","orange","green" ]
 criadorDeDivs ()
 function criadorDeDivs () {
-    console.log(colorPalette)
+    
     for (let cor = 0; cor < cores.length; cor++) {
         const element = cores[cor];
         let divs = document.createElement("div")
@@ -11,8 +12,25 @@ function criadorDeDivs () {
         divs.style.backgroundColor = element
         divs.id = element
         
-        colorPalette.appendChild(divs)
-        
+        colorPalette.appendChild(divs)   
     }
 
 }
+//Requisito4
+let pixelBoard = document.getElementById("pixel-board");
+grid ()
+function grid (){
+    for (let quadrorizonte = 0; quadrorizonte < 5; quadrorizonte++) {
+        let pixel = document.createElement("div")
+        pixel.className = "pixel"
+        pixelBoard.appendChild(pixel)
+        for (let vertiquadro = 0; vertiquadro < 4; vertiquadro++) {
+            let pixel = document.createElement("div")
+            pixel.className = "pixel"
+            pixelBoard.appendChild(pixel)
+        }
+        
+    }  
+}
+
+
